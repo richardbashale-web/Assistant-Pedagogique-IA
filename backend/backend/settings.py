b119799e40a9e62768ef2e1647d7b86375908bf8
richardbashale <richardbashale@gmail.com>
2026-07-20 16:14:12 +0200

    modification de la securisation d'api gemini

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -12,9 +12,13 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 
 import os
 from pathlib import Path
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+# Charger les variables d'environnement depuis le fichier .env
+load_dotenv(BASE_DIR / '.env')
 
 
 # Quick-start development settings - unsuitable for production
