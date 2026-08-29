e4e4a6cac25c43f0d2ab29e90efe227a596db390
richardbashale <richardbashale@gmail.com>
2026-08-29 22:20:50 +0200

    ajoute du secret key

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -33,7 +33,7 @@ load_dotenv(BASE_DIR / ".env")
 # SECURITY
 # ============================================================
 
-SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 DEBUG = os.environ.get("DEBUG", "True").lower() == "true"
 
